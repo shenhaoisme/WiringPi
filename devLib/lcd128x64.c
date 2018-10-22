c3bc2171c296e66cab0d69d4e9a016d177ba9432
shenhaoisme <shenhaoisme@163.com>
2018-10-22 13:26:55 +0800

    some translate

--- devLib/lcd128x64.c
+++ devLib/lcd128x64.c
@@ -9,25 +9,18 @@
  *	This implementation has the Pins from the Pi hard-wired into it,
  *	in particular wiringPi pins 0-7 so that we can use
  *	digitalWriteByete() to speed things up somewhat.
+
+ 基于图形的LCD驱动程序。
+  *这是为了驱动并行接口LCD驱动器
+  *基于通用的12864H芯片
+ *
+  *这些芯片有很多变化，但它们都是大多数
+  *似乎很相似。
+  *这个实现将Pi中的Pins硬连接到其中，
+  *特别是wiringPi引脚0-7，以便我们可以使用
+  * digitalWriteByete（）可以加快速度。
+ *
  *
- * Copyright (c) 2013 Gordon Henderson.
- ***********************************************************************
- * This file is part of wiringPi:
- *	https://projects.drogon.net/raspberry-pi/wiringpi/
- *
- *    wiringPi is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    wiringPi is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
- ***********************************************************************
  */
 
 #include <stdio.h>
--- devLib/scrollPhat.c
+++ devLib/scrollPhat.c
@@ -1,25 +1,7 @@
 /*
  * scrollPhat.c:
  *	Simple driver for the Pimoroni Scroll Phat device
- *
- * Copyright (c) 2015 Gordon Henderson.
- ***********************************************************************
- * This file is part of wiringPi:
- *	https://projects.drogon.net/raspberry-pi/wiringpi/
- *
- *    wiringPi is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    wiringPi is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
- ***********************************************************************
+
  */
 
 #include <stdio.h>
@@ -308,9 +290,10 @@ int scrollPhatPutchar (int c)
 
 /*
  * scrollPhatPuts:
- *	Send a string to the display - and scroll it across.
- *	This is somewhat of a hack in that we print the entire string to the
+ *	Send a string to the display - and scroll滚动 it across.
+ *	这有点像This is somewhat of a hack in that we print the entire string to the
  *	display and let the point clipping take care of what's off-screen...
+ 
  *********************************************************************************
  */
 
